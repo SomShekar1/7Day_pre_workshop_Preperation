93e47e0b3ab27a0e4411adcea5e2c29efbebf19c
Somshekar B <rajsomshekar@gmail.com>
2026-06-03 22:09:58 +0530

    Day 1: Added prime number program

---
+++ Day-1-Python-Core-SQL-Basics/Day-1-Python-Core-SQL-Basics/prime.py
@@ -0,0 +1,13 @@
+# Python program to Check the Number is prime or Not
+num = 3
+
+if num > 1:
+    for i in range(2,num):
+        if (num % i) == 0:
+            print(num,"is not a prime number.")
+            print(i,"times",num//i,"is",num)
+            break
+        else:
+            print(num,"is a prime number.")
+else:
+    print(num,"is not a prime number.")
